fc676f590ace7c24673e6628621b85aeeefa8cd5
hwangheetae(laptop) <tejaia@naver.com>
2025-05-16 16:28:41 +0900

    chore: 페이지 컴포넌트 리팩토링 및 정적 파라미터 반환 방식 개선

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -2,26 +2,21 @@ import TotalPost from "@/components/post/total-post/TotalPost";
 import { getPaths, parsePostDetail } from "@/lib/posts";
 import path from "path";
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ slug: string }>;
-}) {
-  const slug = (await params).slug;
+const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
+  const { slug } = await params;
   const postPath = path.join(process.cwd(), "src", "posts", `${slug}.mdx`);
   const post = await parsePostDetail(postPath);
   return <TotalPost post={post} />;
-}
+};
 
 export const generateStaticParams = async () => {
   const paths = getPaths();
-  const slugs = paths
-    .map((filePath: string) => {
-      const fileName = filePath.split(path.sep).pop();
-      return fileName?.replace(/.mdx/, "");
-    })
-    .map((slug) => ({ slug }));
-  return slugs;
+  return paths.map((filePath: string) => {
+    const fileName = filePath.split(path.sep).pop();
+    return { slug: fileName?.replace(/.mdx/, "") };
+  });
 };
+
+export default Page;
 
 export const dynamicParams = false;
